03e11ab168b2bed123e91a73529aed75eb4e7892
malvin149 <205408852+malvin149@users.noreply.github.com>
2026-08-10 09:32:20 +0000

    Add Book constructor with unique id, and info/toggleRead prototype methods

---
+++ script.js
@@ -0,0 +1,21 @@
+const myLibrary = []
+
+function Book(title, author, pages, read) {
+	if (!new.target) {
+		throw new Error("You must use the 'new' operator to call the constructor")
+	}
+
+	this.id = crypto.randomUUID()
+	this.title = title
+	this.author = author
+	this.pages = pages
+	this.read = read
+}
+
+Book.prototype.info = function () {
+	return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "read" : "not read yet"}`
+}
+
+Book.prototype.toggleRead = function () {
+	this.read = !this.read
+}
