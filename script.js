7628e9744960370631a9d901c200220fcd99b4f1
malvin149 <205408852+malvin149@users.noreply.github.com>
2026-08-12 23:11:24 +0000

    Add Library module (revealing module pattern) to replace global myLibrary array

--- script.js
+++ script.js
@@ -10,14 +10,37 @@ function createBook(title, author, pages, read) {
 		pages,
 		read,
 
-		info: function() {
+		info: function () {
 			return `${title} by ${author}, ${pages} pages, ${read ? "read" : "not read yet"}`
 		},
 		toggleRead: function () {
-			read = !read;
-		}
+			read = !read
+		},
 	}
 }
+
+const Library = (function () {
+	const myLibrary = []
+
+	function addBook(title, author, pages, read) {
+		const book = createBook(title, author, pages, read)
+		myLibrary.push(book)
+	}
+	function removeBook(id) {
+		const idx = myLibrary.findIndex((book) => book.id === id)
+		if (idx === -1) return "Book not found"
+		myLibrary.splice(idx, 1)
+	}
+	function toggleRead(id) {
+		const foundBook = myLibrary.find((book) => book.id === id)
+		foundBook.toggleRead()
+	}
+	function getAllBooks() {
+		return myLibrary
+	}
+
+	return { addBook, removeBook, getAllBooks, toggleRead }
+})()
 
 function addBookToLibrary(title, author, pages, read) {
 	const book = new Book(title, author, pages, read)
